c6cf4c41b09254afd5b76c3535faaadcc462bb80
jamie <jamie7893@live.com>
2016-03-28 00:32:16 -0500

    add and remove skills on edit profile

--- public/js/profile.js
+++ public/js/profile.js
@@ -33,7 +33,7 @@ function drawUsers(user) {
         console.log(userinfo);
         $('#userlist').append('<div id="' + user.id + '" class="flexbox">');
         $('#' + user.id).append('<p align="center"> Click here to upload a profile picture </p></div></div>');
-        $('#' + user.id).append('<img class="img-circle img-responsive img-center" src="/images/' + user.img +'" alt="">');
+        $('#' + user.id).append('<img class="img-circle img-responsive img-center" src="/images/' + user.img + '" alt="">');
         $('#' + user.id).append('<h1 align="center">' + userinfo.title.name + '. ' + user.nameFirst + ' ' + user.nameLast + '</h2>');
         $('#' + user.id).append('<p align="center">' + user.desc + '</p></div></div>');
       });
@@ -84,42 +84,56 @@ function welcome() {
 
 function profile(user) {
   $.ajax({
-    type: "GET",
-    url: 'http://localhost:1738/title',
-    success: function(titles) {
-      $.ajax({
         type: "GET",
-        url: 'http://localhost:1738/skill',
-        success: function(skills) {
-          // Calling Profile Form
-          $("#profile_form").click(function() {
-            $('#userlist').hide();
-            $('input[name=nameFirst]').val(user.nameFirst);
-            $('input[name=nameLast]').val(user.nameLast);
-            var count = 0;
-            $('#newskill').click(function() {
-              $('#thenewskill').append('<input type="text" name="skill" id="skill" placeholder="Skill" required="true" /> <br />');
-            });
-            $.map(titles, function(title) {
-              $('#selectpicker').append('<option>' + title.name + '</option>');
-            });
-            $.map(skills, function(skill) {
-              $('#skillpicker').append('<option>' + skill.name + '</option>');
-            });
-            $('#addskill').click(function() {
-              console.log('click');
-              $('#skills').append('<select name="skill[]" id="skillpicker' + ++count + '" class="form-control"></select><br />');
-              $.map(skills, function(skill) {
-                $('#skillpicker' + count).append('<option>' + skill.name + '</option>');
-              });
+        url: 'http://localhost:1738/title',
+        success: function(titles) {
+          $.ajax({
+              type: "GET",
+              url: 'http://localhost:1738/skill',
+              success: function(skills) {
+                // Calling Profile Form
+                $("#profile_form").click(function() {
+                    $('#userlist').hide();
+                    $('input[name=nameFirst]').val(user.nameFirst);
+                    $('input[name=nameLast]').val(user.nameLast);
+                    var count = 0;
+                    $('#newskill').click(function() {
+                      $('#thenewskill').append('<div id="divskill' + ++count + '"><input type="text" name="skill" id="skill" placeholder="Skill" required="true" /> <br /></div>');
+                      $('#thenewskill').append('<img id="' + count + '" style="cursor: pointer;" class="removebtn3" src="./images/red-minus-sign-icon-12101.png"></img>');
+                      $(".removebtn3").click(function() {
+                        $('#divskill' + this.id).remove();
+                        $(this).remove();
+                        return false;
+                      });
+                    });
+                    $.map(titles, function(title) {
+                      $('#selectpicker').append('<option>' + title.name + '</option>');
+                    });
+                    $.map(skills, function(skill) {
+                      $('#skillpicker').append('<option>' + skill.name + '</option>');
+                    });
+                    $('#addskill').click(function() {
+                      $('#skills').append('<div id="divskill' + ++count + '"><select name="skill[]" id="skillpicker' + count + '" class="form-control"></select><br /></div>');
+                      $('#skillpicker' + count).append('<option id="optremote" disabled selected value> -- select a skill -- </option>');
+                      $('#skills').append('<img id="' + count + '" style="cursor: pointer;" class="removebtn2" src="./images/red-minus-sign-icon-12101.png"></img>');
+                      $(".removebtn2").click(function() {
+                        $('#divskill' + this.id).remove();
+                        $(this).remove();
+                        return false;
+                      });
+                        $.map(skills, function(skill) {
+                          $('#skillpicker' + count).append('<option>' + skill.name + '</option>');
+                        });
 
-            });
-            $(".user_profile").show();
-            $(".header_title").text('Profile Setup');
-            return false;
+                      });
+                      $('textarea[name=desc]').val(user.desc);
+
+                      $(".user_profile").show();
+                      $(".header_title").text('Profile Setup');
+                      return false;
+                    });
+                  }
+                });
+            }
           });
-        }
-      });
-    }
-  });
-}
+      }

6cec9989bbd01d728fbba3794d5fd5c3c1b431c9
jamie <jamie7893@live.com>
2016-04-03 19:19:39 -0500

    broken ng-click

--- public/js/angular.js
+++ public/js/angular.js
@@ -3,24 +3,33 @@
   angular.module('skilly', ['mgcrea.ngStrap']);
   var myApp = angular.module('skilly');
 
-  myApp.factory('mainInfo', function($http) {
+  myApp
 
-  });
+    .factory('mainInfo', function($http) {
 
-  myApp.controller('GetAllUsers', ['$scope', '$http', function($scope, $http) {
+  })
+
+  .controller('GetAllUsers', ['$scope', '$http', function($scope, $http) {
     var self = this;
-    $http({
-    method: 'GET',
-    url: 'http://localhost:1738/user'
-  }).then(function successCallback(response) {
+    self.theSkills = true;
 
-    var auser = $.map(response.data, function(data) {
-    });
-    self.users = response.data;
-        console.log(response.data);
+   $scope.clickme = function() {
+     console.log('hello');
+   };
+
+    $http({
+      method: 'GET',
+      url: 'http://localhost:1738/user'
+    }).then(function successCallback(response) {
+
+      var auser = $.map(response.data, function(data) {});
+      self.users = response.data;
+      console.log(response.data);
     }, function errorCallback(response) {
 
     });
+
+
   }]);
 
 
